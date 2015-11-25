3c16d5a59d266809698c177166874e0658517cc6
askl56 <amscott@openmailbox.org>
2015-11-25 14:17:58 +0000

    Added routing:

--- app/App.js
+++ app/App.js
@@ -1,3 +1,7 @@
-var React = require("react");
+var React = require('react');
 var Router = require('react-router');
-var routes = require('./config/routes')
+var routes = require('./config/routes');
+
+Router.run(routes, function(Root){
+  React.render(<Root />, document.getElementById('app'));
+});
