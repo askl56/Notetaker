47f4de8c2019c8cfae4d272050a6cab1b6464593
askl56 <amscott@openmailbox.org>
2015-11-25 13:55:52 +0000

    Bug in webpack fixed

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ module.exports = {
   output: {
     filename: "public/bundle.js"
   },
-
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel'
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }
+    ]
   }
 };
